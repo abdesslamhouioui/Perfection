4b4a4629afbc7000f23497c55b0cf4b49ed83282
Abdesslam Haouioui <abdesslam009@live.fr>
2022-12-29 17:40:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const app = express()
 app.use(express.json())
 app.use("/user", require('./route/RouteUser'))
 app.use("/service", require('./route/RouteService'))
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static('./client/build'))
-    app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-    })
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static('./client/build'))
+//     app.get('/*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+//     })
+// }
 app.listen(process.env.PORT, (err) => {
     err ? console.log(err) : console.log(`server is running at port ${process.env.PORT}`)
 })
